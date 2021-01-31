bd46295eb46bbc9b1f422869ef83f8c7bcfe3b5c
Ryan K. Clark <mail.w3geek@gmail.com>
2021-01-31 09:53:29 -0500

    Task #1 Say hello to user with a personal greeting when user's name is provided.

--- App.cpp
+++ App.cpp
@@ -1,11 +1,23 @@
 #include "App.h"
 
 #include <iostream>
+#include <stdexcept>
+#include <stdio.h>
 #include <string>
 
 using namespace W3Geek;
 using namespace std;
 
 void App::sayHello(string user) {
-	cout << "Hello, " << user << "!" << endl;
+	const char *name = user.c_str();
+	const char *format = "Hello, %s!";
+	int length = (snprintf(nullptr, 0, format, name) + 1); // Extra space for '\0' (NULL Termination Character)
+	if (length <= 0) {
+		throw runtime_error("Error occured during formating.");
+	}
+
+	char *buffer = new char[length];
+	snprintf(buffer, length, format, name);
+	string message = string(buffer, (length - 1)); // Remove '\0'
+	cout << message << endl;
 }
